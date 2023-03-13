f6a12acb8c900acea9692b95f7f15cc8571b77b0
Matina Shrestha <104951387+mshrestha5@users.noreply.github.com>
2023-03-13 11:39:46 -0500

    contact  section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,55 @@
 import React from "react";
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsFillTelephoneFill } from "react-icons/bs";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In touch</h5>
+      <h2>Contact</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>matinashrestha92@gmail.com</h5>
+            <a
+              href="mailto:matinashrestha92@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a Message
+            </a>
+          </article>
+
+          <article className="contact__option" target="_blank" rel="noreferrer">
+            <BsFillTelephoneFill className="contact__option-icon" />
+            <h4>Phone</h4>
+            <h5>4097976471</h5>
+            <a href="tel:4097976471" target="_blank" rel="noreferrer">
+              call
+            </a>
+          </article>
+        </div>
+
+        <form action="">
+          <input type="text" name="name" placeholder="Full Name" required />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            required
+          />
+          <textarea name="message" row="7" placeholder="Message" required />
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
