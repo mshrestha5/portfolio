b79b984f7d8b931c041ee4e166065fc9e1c7b8ef
Matina Shrestha <104951387+mshrestha5@users.noreply.github.com>
2023-03-13 02:13:10 -0500

    about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import "./about.css";
+import AI from "../../assets/ai.jfif";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5> Get to know</h5>
+      <h2>Me</h2>
+      <div className="container  about__container">
+        <div className="about__me">
+          <div className="about_me-image">
+            <img src={AI} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small> 3+ years</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small> WorldWide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small> 20+ Completed</small>
+            </article>
+
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Necessitatibus eaque dolores ipsam. A sit, unde perspiciatis minus
+              quas tenetur! Nesciunt, rem? Totam quae amet soluta eius aliquam
+              repudiandae exercitationem iure.
+            </p>
+
+            <a href="#contact" className="btn btn-primary">
+              Let's talk
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,23 +5,45 @@ import { AiOutlineUser } from "react-icons/ai";
 import { BiBook } from "react-icons/bi";
 import { RiServiceLine } from "react-icons/ri";
 import { BiMessageSquareDetail } from "react-icons/bi";
+import { useState } from "react";
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <BiBook />
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <RiServiceLine />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <BiMessageSquareDetail />
       </a>
     </nav>
