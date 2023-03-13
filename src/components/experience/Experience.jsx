3174512100e00a2227ea9a0670abed99f72e572b
Matina Shrestha <104951387+mshrestha5@users.noreply.github.com>
2023-03-13 08:46:29 -0500

    experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,11 +1,96 @@
 import React from "react";
 import "./experience.css";
+import { BsFillPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
     <section id="experience">
-      <h5>Skills</h5>
+      <h5>Skills I Have</h5>
       <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experince__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Expereinced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experince__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Expereinced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experince__details-icon" />
+              <div>
+                <h4>javaScript</h4>
+                <small className="text-light">Expereinced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Expereinced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Tailwind</h4>
+                <small className="text-light">Expereinced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* end of fe */}
+        <div className="experience__backend">
+          {" "}
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>NodeJs</h4>
+                <small className="text-light">Expereinced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Expereinced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Java</h4>
+                <small className="text-light">Expereinced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Laravel</h4>
+                <small className="text-light">Expereinced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Tailwind</h4>
+                <small className="text-light">Expereinced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./services.css";
 
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+    </section>
+  );
 };
 
 export default Services;
